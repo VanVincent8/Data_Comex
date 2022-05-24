34ed397a67fa402829c0787d12eb335e1d5e30a3
VanVincent8 <104674754+VanVincent8@users.noreply.github.com>
2022-05-24 10:20:18 +0200

    Update Exportacions industrials per àrees.R
    24052022 - v.2.1
    - Versió final

--- Exportacions industrials per àrees.R
+++ Exportacions industrials per àrees.R
@@ -72,110 +72,129 @@ switching_frames <- function(n) {
   }
 }
 
+seleccio_criteris <- function() {
+  # Naveguem a la url
+  remDr$navigate(url)
+  
+  # Premem en el botó d'entrar a Datos Comex España
+  remDr$findElement(using = "css selector", value = "ul.row > li:nth-child(1) > ul > li:nth-child(1) > a")$clickElement()
+  
+  # Seleccionem els criteris
+  
+  # Exportacions
+  Sys.sleep(1.5)
+  switching_frames(0)
+  remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(3) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(2) ")$clickElement()
+  
+  # Territorial
+  switching_frames(0)
+  
+  remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(6) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(2) ")$clickElement()
+  
+  remDr$findElement(using = "css selector", value = "#FormInforme > table > tbody > tr > td >table >tbody > tr:nth-child(6) > td > div > table > tbody > tr > td:nth-child(3) > i")$clickElement()
+  Sys.sleep(1.5)
+  
+  switching_frames(3)
+  
+  remDr$findElement(using = "css selector", value = "#check10")$clickElement()
+  
+  # País
+  switching_frames(0)
+  
+  remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(5) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(1) ")$clickElement()
+  
+  remDr$findElement(using = "css selector", value = "#FormInforme > table > tbody > tr > td >table >tbody > tr:nth-child(5) > td > div > table > tbody > tr > td:nth-child(3) > i")$clickElement()
+  
+  switching_frames(3)
+  Sys.sleep(1.5)
+  
+  #### <<< Total món >>>
+  remDr$findElement(using = "css selector", value = "#check2")$clickElement()
+  
+  #### <<< Amèrica >>>
+  for (i in 6:9){  
+    check_box <- str_c("#check", i)
+    remDr$findElement(using = "css selector", value = check_box)$clickElement()
+  }
+  
+  #### <<< Àsia >>>
+  Sys.sleep(1.5)
+  remDr$findElement(using = "css selector", value = "#check11")$clickElement()
+  
+  remDr$findElement(using = "css selector", value = "#raiz > table > tbody > tr:nth-child(12) > td > i:nth-child(2)")$clickElement()
+  
+  Sys.sleep(1.5)
+  remDr$findElement(using = "css selector", value = "#check56")$clickElement()
+  remDr$findElement(using = "css selector", value = "#check59")$clickElement()
+  
+  #### <<< Europa >>>
+  Sys.sleep(2)
+  remDr$findElement(using = "css selector", value = "#check67")$clickElement()
+  
+  Sys.sleep(2)
+  remDr$findElement(using = "css selector", value = "#raiz > table > tbody > tr:nth-child(78) > td > i:nth-child(2)")$clickElement()
+  
+  Sys.sleep(2)
+  remDr$findElement(using = "css selector", value = "#check78")$clickElement()
+  
+  remDr$findElement(using = "css selector", value = "#check298")$clickElement()
+  
+  Sys.sleep(2)
+  remDr$findElement(using = "css selector", value = "#raiz > table > tbody > tr:nth-child(248) > td > i:nth-child(2)")$clickElement()
+  
+  Sys.sleep(1.5)
+  remDr$findElement(using = "css selector", value = "#check299")$clickElement()
+  remDr$findElement(using = "css selector", value = "#check302")$clickElement()
+  remDr$findElement(using = "css selector", value = "#check303")$clickElement()
+  remDr$findElement(using = "css selector", value = "#check307")$clickElement()
+  
+  # Dates
+  Sys.sleep(1.5)
+  switching_frames(0)
+  
+  remDr$findElement(using = "css selector", value = "#FormInforme > table > tbody > tr > td >table >tbody > tr:nth-child(7) > td > table > tbody > tr > td:nth-child(3) > i")$clickElement()
+  
+  switching_frames(3)
+  
+  remDr$findElement(using = "css selector", value = "#Table1 > tbody > tr:nth-child(5) > td:nth-child(2) > p > a")$clickElement()
+  
+  Sys.sleep(2)
+  for (i in (year-1995+1):(year-1995+2)){  
+    check_box <- str_c("#check", i)
+    remDr$findElement(using = "css selector", value = check_box)$clickElement()
+  }
+  
+  # Unitats de mesura
+  Sys.sleep(2)
+  switching_frames(0)
+  
+  remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(10) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(4) ")$clickElement()
+  
+  # Informe
+  Sys.sleep(2)
+  switching_frames(0)
+  remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(14) > td > table > tbody > tr >td:nth-child(2) > select > option:nth-child(4) ")$clickElement()
+  
+  # Veim l'informe
+  Sys.sleep(2)
+  switching_frames(0)
+  
+  remDr$findElement(using = "id", value = "enviarDatos")$clickElement()
+}
+
 ######################################################################################################################
 # Quin any i mesos es cerquen?
-year <- as.numeric(readline("Quin és l'últim any disponible a la web? (Introduir l'any en format yyyy): "))
-months <- as.numeric(readline("Número de mesos que es volen extreure: "))
+year <- as.numeric(readline("\nQuin és l'any que es vol extreure? (Introduir l'any en format yyyy): "))
+months <- as.numeric(readline("\nNúmero de mesos que es volen extreure: "))
 
 # Pàgina principal del Data Comex
 url <- "https://comercio.serviciosmin.gob.es/Datacomex/"
 
 # A vegades pot no funcionar. En aquest cas canviar el nombre del port, ex: passar de 5556 a 5557
-rD <- rsDriver(browser="firefox", port=5558L, verbose=F)
+rD <- rsDriver(browser="firefox", port=5557L, verbose=F)
 remDr <- rD[["client"]]
 
-# Naveguem a la url
-remDr$navigate(url)
-
-# Premem en el botó d'entrar a Datos Comex España
-remDr$findElement(using = "css selector", value = "ul.row > li:nth-child(1) > ul > li:nth-child(1) > a")$clickElement()
-
-# Seleccionem els criteris
-
-# Exportacions
-switching_frames(0)
-remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(3) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(2) ")$clickElement()
-
-# Territorial
-switching_frames(0)
-
-remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(6) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(2) ")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#FormInforme > table > tbody > tr > td >table >tbody > tr:nth-child(6) > td > div > table > tbody > tr > td:nth-child(3) > i")$clickElement()
-
-switching_frames(3)
-
-remDr$findElement(using = "css selector", value = "#check10")$clickElement()
-
-# País
-switching_frames(0)
-
-remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(5) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(1) ")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#FormInforme > table > tbody > tr > td >table >tbody > tr:nth-child(5) > td > div > table > tbody > tr > td:nth-child(3) > i")$clickElement()
-
-switching_frames(3)
-
-#### <<< Total món >>>
-remDr$findElement(using = "css selector", value = "#check2")$clickElement()
-
-#### <<< Amèrica >>>
-for (i in 6:9){  
-  check_box <- str_c("#check", i)
-  remDr$findElement(using = "css selector", value = year)$clickElement()
-}
-
-#### <<< Àsia >>>
-remDr$findElement(using = "css selector", value = "#check11")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#raiz > table > tbody > tr:nth-child(12) > td > i:nth-child(2)")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#check56")$clickElement()
-remDr$findElement(using = "css selector", value = "#check59")$clickElement()
-
-#### <<< Europa >>>
-remDr$findElement(using = "css selector", value = "#check67")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#raiz > table > tbody > tr:nth-child(78) > td > i:nth-child(2)")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#check78")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#check298")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#raiz > table > tbody > tr:nth-child(248) > td > i:nth-child(2)")$clickElement()
-
-remDr$findElement(using = "css selector", value = "#check299")$clickElement()
-remDr$findElement(using = "css selector", value = "#check302")$clickElement()
-remDr$findElement(using = "css selector", value = "#check303")$clickElement()
-remDr$findElement(using = "css selector", value = "#check307")$clickElement()
-
-# Dates
-switching_frames(0)
-
-remDr$findElement(using = "css selector", value = "#FormInforme > table > tbody > tr > td >table >tbody > tr:nth-child(7) > td > table > tbody > tr > td:nth-child(3) > i")$clickElement()
-
-switching_frames(3)
-
-remDr$findElement(using = "css selector", value = "#Table1 > tbody > tr:nth-child(5) > td:nth-child(2) > p > a")$clickElement()
-
-for (i in (year-1995+1):(year-1995+2)){  
-  check_box <- str_c("#check", i)
-  remDr$findElement(using = "css selector", value = check_box)$clickElement()
-}
-
-# Unitats de mesura
-switching_frames(0)
-
-remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(10) > td > div > table > tbody > tr >td:nth-child(2) > select > option:nth-child(4) ")$clickElement()
-
-# Informe
-switching_frames(0)
-remDr$findElement(using = "css selector", value = "td.panelClass2 > table > tbody > tr:nth-child(14) > td > table > tbody > tr >td:nth-child(2) > select > option:nth-child(4) ")$clickElement()
-
-# Veim l'informe
-switching_frames(0)
-
-remDr$findElement(using = "id", value = "enviarDatos")$clickElement()
+seleccio_criteris()
 
 ######################################################################################################################
 # Creació de la taula final i l'excel
